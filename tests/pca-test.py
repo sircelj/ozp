4f514592845f68010ba8dff711063ae14cf5d7a5
BlazZupan <blaz.zupan@fri.uni-lj.si>
2018-03-10 07:30:27 +0100

    Method-based testing.

--- tests/pca-test.py
+++ tests/pca-test.py
@@ -3,7 +3,7 @@ import sklearn
 import numpy as np
 from sklearn import datasets
 
-from pca import PowerPCA, OrtoPCA, EigenPCA
+from pca import EigenPCA, PowerPCA, OrtoPCA
 
 
 class PCATest(unittest.TestCase):
@@ -12,39 +12,51 @@ class PCATest(unittest.TestCase):
         self.X = X - X.mean(axis=0)
         self.n_comp = 3
 
-    def test_pca(self):
+    def fit_test(self, method):
         s_pca = sklearn.decomposition.PCA(self.n_comp)
         s_pca.fit(self.X)
 
-        for my_pca in (PowerPCA, OrtoPCA, EigenPCA):
-            my_pca = my_pca(n_components=self.n_comp)
-            my_pca.fit(self.X)
+        my_pca = method(n_components=self.n_comp)
+        my_pca.fit(self.X)
 
-            self.assertEqual(my_pca.explained_variance_.shape, (self.n_comp, ))
-            np.testing.assert_array_almost_equal(my_pca.explained_variance_,
-                                                 s_pca.explained_variance_, decimal=1)
-            np.testing.assert_array_almost_equal(my_pca.explained_variance_ratio_,
-                                                 s_pca.explained_variance_ratio_, decimal=1)
+        self.assertEqual(my_pca.explained_variance_.shape, (self.n_comp, ))
+        np.testing.assert_array_almost_equal(my_pca.explained_variance_,
+                                             s_pca.explained_variance_, decimal=1)
+        np.testing.assert_array_almost_equal(my_pca.explained_variance_ratio_,
+                                             s_pca.explained_variance_ratio_, decimal=1)
 
-            diff = sum(min(np.linalg.norm(r1 - r2), np.linalg.norm(r1 + r2))
-                       for r1, r2 in zip(my_pca.components_, s_pca.components_))
-            self.assertAlmostEqual(diff, 0.)
+        diff = sum(min(np.linalg.norm(r1 - r2), np.linalg.norm(r1 + r2))
+                   for r1, r2 in zip(my_pca.components_, s_pca.components_))
+        self.assertAlmostEqual(diff, 0.)
 
-    def test_transform(self):
+    def transform_test(self, method):
         n_sample = 5
         train, test = self.X[:-n_sample], self.X[-n_sample:]
+
         s_pca = sklearn.decomposition.PCA(self.n_comp)
         s_pca.fit(train)
         sk_T = s_pca.transform(test)
 
-        for my_pca in (PowerPCA, OrtoPCA, EigenPCA):
-            my_pca = my_pca(self.n_comp)
-            my_pca.fit(train)
+        my_pca = method(self.n_comp)
+        my_pca.fit(train)
 
-            my_T = my_pca.transform(self.X[-n_sample:])
-            self.assertEqual(my_T.shape, (n_sample, self.n_comp))
-            for c1, c2 in zip(sk_T.T, my_T.T):
-                np.testing.assert_almost_equal(min(np.linalg.norm(c1 - c2), np.linalg.norm(c1 + c2)), 0.)
+        my_T = my_pca.transform(self.X[-n_sample:])
+        self.assertEqual(my_T.shape, (n_sample, self.n_comp))
+        for c1, c2 in zip(sk_T.T, my_T.T):
+            np.testing.assert_almost_equal(min(np.linalg.norm(c1 - c2), np.linalg.norm(c1 + c2)), 0.)
+
+    def method_test(self, method):
+        self.fit_test(method)
+        self.transform_test(method)
+
+    def test_EigenPCA(self):
+        self.method_test(EigenPCA)
+
+    def test_PowerPCA(self):
+        self.method_test(PowerPCA)
+
+    def test_OrtoPCA(self):
+        self.method_test(OrtoPCA)
 
 if __name__ == "__main__":
-    unittest.main(verbosity=3)
+    unittest.main(verbosity=2)
